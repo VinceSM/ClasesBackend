f983937e4620f2381b45fd67293eb5df63c7dcc7
VinceSM <vsmx427@gmail.com>
2025-09-07 14:41:11 -0500

    conexion y cors en raiz proyecto

--- conexion.php
+++ conexion.php
@@ -1,12 +1,14 @@
 <?php
-$host = '127.0.0.1';
-$user = 'root';
-$pass = '';
-$db = 'clases';
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "reservascanchas";
 
-$conexion = new mysqli($host, $user, $pass, $db);
+$conexion = new mysqli($host, $user, $pass, $dbname);
 
 if ($conexion->connect_error) {
-    die("Connection failed: " . $conexion->connect_error);
+    die("Error de conexión: " . $conexion->connect_error);
 }
+
+$conexion->set_charset("utf8");
 ?>
